824d8e2dac1fa92fa24cde4f57393a0bef1de555
Henry <157012315+henry-symbios@users.noreply.github.com>
2024-02-06 07:54:32 -0800

    Add comment to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import '../styles/globals.css';
 
+// This is the main application
 const Application = ({ Component, pageProps }) => {
   return (
     <NextUIProvider>
