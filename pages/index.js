1c7c8ba50ed185df9eeadd68bb4268f732ea435a
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-03-08 11:44:26 -0800

    Add header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Article from './posts/article';
+import Header from './components/header';
 import Footer from './components/footer';
 
 export default function Index() {
   return (
     <>
+      <Header />
       <Article />
       <Footer />
     </>
