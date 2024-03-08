13ccb51882959c9ae8223e54a8e6790d77e505a2
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-03-08 11:22:21 -0800

    Added footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Article from './posts/article';
+import Footer from './components/footer';
 
 export default function Index() {
   return (
-    <Article />
+    <>
+      <Article />
+      <Footer />
+    </>
   );
 }
