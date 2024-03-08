719ae262452db69f9435ffff584d4063c5c3d6fd
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-03-08 11:23:03 -0800

    Fix: Remove broken footer

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Article from './posts/article';
-import Footer from './components/footer';
 
 export default function Index() {
   return (
-    <>
-      <Article />
-      <Footer />
-    </>
+    <Article />
   );
 }
