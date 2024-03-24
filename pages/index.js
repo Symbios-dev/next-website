fd05ae2bda35d47d057d3207d2b1ba45ce1a415e
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-03-24 16:06:20 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Article from './posts/article';
-import Header from './components/header';
 import Footer from './components/footer';
+import Header from './components/header';
 
 export default function Index() {
   return (
