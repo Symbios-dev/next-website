b81358316ee1d3c0b49c30239c702a6749fafc50
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-04-16 10:58:06 -0700

    Added link to home data

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -4,6 +4,7 @@ export default () => {
   return (
     <section className='border-3 border-black border-solid w-full h-unit-9xl flex justify-center items-center'>
         <Link href="/posts/article"><h1 className='text-2xl'>Home content</h1></Link>
+        <Link><h1 className='text-2xl'>Home data</h1></Link>
     </section>
   );
 }
