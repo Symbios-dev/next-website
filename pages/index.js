229fbf596fdb6d1b90f2f03f3ed7702494bac5d9
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-04-16 10:16:03 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Article from './posts/article';
 import Footer from './components/footer';
+import Banner from './Banner';
 
 export default function Index() {
+  console.log("Adding the banner");
   return (
     <>
       <Article />
