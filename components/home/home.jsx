a08df4d949e684c9d64c28b5676ff9af6b0dca48
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-04-16 11:49:28 -0700

    Updated home.jsx

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 
 export default () => {
+  console.log("hello world")
   return (
     <section className='border-3 border-black border-solid w-full h-unit-9xl flex justify-center items-center'>
-        <Link href="/posts/article"><h1 className='text-2xl'>Home content</h1></Link>
+        <Link><h1 className='text-2xl'>Home content</h1></Link>
     </section>
   );
 }
