2db018f00fafda58103c027a9a3540177df18fe6
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-05-07 18:13:11 -0700

    Update home.jsx

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -4,7 +4,7 @@ export default () => {
   console.log("hello world")
   return (
     <section className='border-3 border-black border-solid w-full h-unit-9xl flex justify-center items-center'>
-        <Link><h1 className='text-2xl'>Home content</h1></Link>
+        <Link><h1 className='text-2xl'>Home content!</h1></Link>
     </section>
   );
 }
