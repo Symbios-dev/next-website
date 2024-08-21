e63dc135fc07629ac8e9bd2fdda72836cc447a2c
Wanda <156891560+wanda-symbios@users.noreply.github.com>
2024-08-21 13:19:13 -0700

    Update home.jsx
    Fix missing link

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -2,8 +2,9 @@ import Link from 'next/link';
 
 export default () => {
   return (
+    <Header />
     <section className='border-3 border-black border-solid w-full h-unit-9xl flex justify-center items-center'>
-      <Header />
+      <Link href="/posts/article"><h1 className='text-2xl'>Home content</h1></Link>
     </section>
   );
 }
